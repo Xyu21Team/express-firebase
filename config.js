96f9aa865e41700842a1a18b97f4675cb5ff919e
Xyu21Team <srihandayanispt1@gmail.com>
2024-07-28 16:09:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,17 @@
 module.exports = {
     creator: 'CAF-ID',
-    serviceAccount: { note: 'ini json' },
+    serviceAccount: {
+  "type": "service_account",
+  "project_id": "apire-23193",
+  "private_key_id": "f77dcb7535b34c438b83ab2155c179bf741613d4",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC2jQgWiuRMLRDC\nqXJTHWgwsnKpLvyn0w2yScX9xzYxTvPRmb1FF1vsfB/YW7Mv3uU1kYVXats9YeY+\nR+Pa+j7FV55E1ukLI2VOuOw3Ef1h2rooqaJEAbVCtumWF4qBbZ0Tv8rPjSeoD8gP\np4JgpwfJj0jg4x+6L1yUVuKmUsXwM0NjDh/syUdBGMx5bq3cfAHd/pSV6VtM/ORt\nakGR+mqarlXtpkdXLXkrEsHuKLbae9JMHjN025cVNbh5kSLRIpCxxScqfgM1q622\nGIvRjnTT6DRCXxXz2ooVe5EHYiA1CeRsjTwdbPp2L3FY+JmrfCxttyQQFjYsS/2W\nff7iloclAgMBAAECggEAKQ/ZGWT9jEr4waimwB5VwNpq4zZf+4oEQy3H/NkSDg5R\n1rxLsB5cfXnAGgUqbaKoESAeC7dMAGckiK7YCHoQo4gdYCOtQOWh1I7RB2QjCB9Q\nj3P0vXJa6NpG+Y1rCMj0s6wI/R2lpW6DVE4cDAyWOlbiED/Wc6VSoEVY7wugXXJw\nkwfA+4sKGCVm5rCUM5nTQn3+5q5h68gERWNpmgxxUaql3hcG0SYaiZke2MaZi12f\nDIgcd0dA6iyKmtQcMxVYFKDfG6361kKD++UyBuqruhANU34vaTLHEXDFqzCFeDrL\nZWFPtYU7CyrGeP7ffh3++wP897Al316jpJAm3pQBQwKBgQDzAzX+XeldmIBTPpsD\nLc7AxsOjEb99mKAS7BiWHLKeqtWjbcXAzwpz9871xrtFA1htPGKxfvy1D4yZlH7s\nNeWoSdaG3KLB22I0RSDArUXB7u4X4aePbxW5OtaJJZfdvo798pBkoARfLR1w80HS\nCcGZNUrXuXkZllMKepX16JQJiwKBgQDATpyOl/uTBEU833TOoqi5Y0VxZ5d18LFj\nvJgvMCnKY7H6s/WXjli0MX2ivPDE4FAzS/Rqd1sfyQuq5IDF6CIv/19lgI4HVmz3\nVBpGYsfS3G/Rsd4j6rZYauFXPrh5YNQvYmavhDMiwA5jExbq+Lt8Ly0czgCm9ROq\nu6IYTEzoDwKBgQDDxqxzFrlxG/EWH3PGpdrRV9A4yY2Z46HiXK6aENieF0cx+Le+\nU446S0RCoS4xtTZCGsnv8TwpqjM9b3KpXep9H69iytGcl9+P+LkgQPfX+2N5feP+\nHCJGLe60YsLsdB7C2+4UAw5ClxgG7RLz4cREZ7i5/sRfJsoTwRqbGBpbbQKBgATX\nddWZOfo/l0arEZPKQJk6SJtMUmHjx3ONYf9+JO81h1uagEpqiQ0wWctpGl66jwpb\nlTHBA4hPVX3d9ugEqievNEWn8Ot6X4/yjB7Nid5jMmWDJuacmC6tsjKyH47ewzjW\nL2On3mm3kCUqa99Kl93OikI5ZYbOkMoxoykdcqo9AoGAQZ+Tkx9t3G0oKePqCYH1\n4Qq3QxSgGJ1ZUUeaguqa+sadB1fgWtE/rSaYjeyVAL4IlkAz4DYZg57VtrlOE7NI\n7zLBronbb83G7zyt5c3jHYbgtfe229vL7ASQdYX+5ZLv/LNBgQdghNHk2qMVsYZ1\nRo+DZFVvkTmRIKki+pHoF4Y=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-128rs@apire-23193.iam.gserviceaccount.com",
+  "client_id": "117890807989850734039",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-128rs%40apire-23193.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+},
     storageBucket: '',
 }
